14b4d4c8ab4105238a5bbaa0ec6e13125d4a274f
Vijaya Ragavan <vijay.harry11@gmail.com>
2019-08-09 00:10:56 +0530

    New Exercises

--- venv/Ex 3.py
+++
@@ -1,8 +0,0 @@
-#Given a range of numbers. Iterate from o^th number to the end number and print the sum of the current number and previous number
-s = str(input('Enter the string'))
-
-def PrintEvenIndex(str):
-    for i in range(0, len(str)-1, 2):
-        print(str[i])
-
-PrintEvenIndex(s)
